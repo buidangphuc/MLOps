567fba04001d5290ea0ad8b66a7839b0264fddf7
Bùi Đăng phúc <95173128+buidangphuc@users.noreply.github.com>
2025-04-06 10:10:19 +0700

    feat: implement data ingestion pipeline with file download and extraction functionality

---
+++ basics_ds_project/main.py
@@ -0,0 +1,12 @@
+from src.datascience import log
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   log.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   log.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
