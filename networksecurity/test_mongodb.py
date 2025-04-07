c42e1075d06a02ecc656d89171c4ad3fcb8bdc5f
Bùi Đăng phúc <95173128+buidangphuc@users.noreply.github.com>
2025-04-07 10:47:00 +0700

    feat: add MongoDB integration for data extraction and insertion; implement CSV to JSON conversion and connection testing

---
+++ networksecurity/test_mongodb.py
@@ -0,0 +1,16 @@
+
+from pymongo.mongo_client import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+uri = os.getenv("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
