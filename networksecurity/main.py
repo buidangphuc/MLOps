53f010377afe4cfbd69a3b06d16aaa333649beee
Bùi Đăng phúc <95173128+buidangphuc@users.noreply.github.com>
2025-04-07 11:27:38 +0700

    feat: add data validation module; implement configuration, constants, and YAML file handling

--- networksecurity/main.py
+++ networksecurity/main.py
@@ -1,7 +1,12 @@
 import sys
 
 from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import (DataIngestionConfig, TrainingPipelineConfig)
+from src.components.data_validation import DataValidation
+from src.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    TrainingPipelineConfig,
+)
 from src.exception.exception import NetworkSecurityException
 from src.log import log
 
@@ -14,6 +19,13 @@ if __name__ == "__main__":
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         log.info("Data Initiation Completed")
         print(dataingestionartifact)
+
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        log.info("Initiate the data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        log.info("data Validation Completed")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
