c35d2e49aee4d0578e32e0bf90b456d49204e55e
Bùi Đăng phúc <95173128+buidangphuc@users.noreply.github.com>
2025-04-07 11:34:36 +0700

    feat: add data transformation module; implement configuration, constants, and file handling for transformed data

--- networksecurity/main.py
+++ networksecurity/main.py
@@ -1,9 +1,11 @@
 import sys
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 from src.components.data_validation import DataValidation
 from src.entity.config_entity import (
     DataIngestionConfig,
+    DataTransformationConfig,
     DataValidationConfig,
     TrainingPipelineConfig,
 )
@@ -26,6 +28,17 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         log.info("data Validation Completed")
         print(data_validation_artifact)
+
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        log.info("data Transformation started")
+        data_transformation = DataTransformation(
+            data_validation_artifact, data_transformation_config
+        )
+        data_transformation_artifact = (
+            data_transformation.initiate_data_transformation()
+        )
+        print(data_transformation_artifact)
+        log.info("data Transformation completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
